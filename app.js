3e478769f5e126370e52bb8f0a3bf022ee831d91
davehernandez92 <davehgal@gmail.com>
2022-09-14 13:21:58 -0300

    change port for deployment

--- app.js
+++ app.js
@@ -26,11 +26,7 @@ app.post('/submit', function(req, res) {
 })
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
-app.listen(port, function() {
-  console.log("Server up and running");
-});
+
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
